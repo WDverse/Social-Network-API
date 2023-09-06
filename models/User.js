f45a95e0463ff73c1a2cb453be93d6f8343799a1
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-05 22:33:54 -0400

    corrected db name, changed mongodb version and created user model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, Model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trimmed: [true, "Username required"],
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: function (email) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+    required: [true, "User email required"],
+  },
+
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thoughts",
+    },
+  ],
+
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our User model
+const User = model("User", userSchema);
+
+module.exports = User;
